5d18042111be2682b0e39fcdac690eeadb1b1b27
Dafa ahmad sajangah <dafaahmad145@gmail.com>
2025-03-18 19:21:38 +0700

    tambah laravel breeze + sanctum
    kur nambahi tok

--- routes/web.php
+++ routes/web.php
@@ -1,19 +1,9 @@
 <?php
 
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\DashboardController;
 use Illuminate\Support\Facades\Route;
 
-//default route
-// Route::get('/dashboard', function () {
-//     return view('dashboard');
-// });
-//registrasi & login route
-Route::get('/registrasi', [AuthController::class, 'tampilRegistrasi'])->name('registrasi.tampil');
-Route::post('/registrasi/submit', [AuthController::class, 'submitRegistrasi'])->name('registrasi.submit');
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
 
-Route::get('/login', [AuthController::class, 'tampilLogin'])->name('login.tampil');
-Route::post('/login/submit', [AuthController::class, 'submitLogin'])->name('login.submit');
-
-//route dashboard
-Route::get('/dashboard', [DashboardController::class, 'tampilDashboard'])->name('tampil.dashboard');
+require __DIR__.'/auth.php';
