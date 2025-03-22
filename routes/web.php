ca3f95752fe01aa1c107b077bd368c5b428aa9f9
Dafa ahmad sajangah <dafaahmad145@gmail.com>
2025-03-22 12:43:41 +0700

    ngubah sepetil

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,12 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return view('index');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/registrasi', [AuthController::class, 'tampilRegistrasi'])->name('registrasi.tampil');
+
+require __DIR__ . '/auth.php';
